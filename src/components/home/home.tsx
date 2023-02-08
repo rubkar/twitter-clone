b0ae38766d89668962283929e3756b24346715b0
Ruben K <ruben.karapetyan96@gmail.com>
2023-02-08 18:09:04 +0400

    added tweet post and action bar

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,6 +1,11 @@
 import react from 'react';
-import TwitterIcon from '@mui/icons-material/Twitter';
 import './home.css';
+import InsertPhotoOutlinedIcon from '@mui/icons-material/InsertPhotoOutlined';
+import GifBoxOutlinedIcon from '@mui/icons-material/GifBoxOutlined';
+import BallotOutlinedIcon from '@mui/icons-material/BallotOutlined';
+import SentimentSatisfiedOutlinedIcon from '@mui/icons-material/SentimentSatisfiedOutlined';
+import PendingActionsOutlinedIcon from '@mui/icons-material/PendingActionsOutlined';
+import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
 
 export const HomeContent = () => {
   return (
@@ -10,11 +15,50 @@ export const HomeContent = () => {
         <div className="navigation-panel">
           <div className="for-you-wrapper">
             <div className="fy-button">
-            <span>For you</span>
-            <div className="selector-status"></div>
+              <span>For you</span>
+              <div className="selector-status"></div>
             </div>
           </div>
           <div className="following">Following</div>
+        </div>
+      </div>
+      <div className="tweet-post-bar ">
+        <div className="account-posting-bar">
+          <img
+            src="https://pbs.twimg.com/profile_images/1621111347168661510/ohqli9_A_normal.jpg"
+            alt="pic"
+            className="profile-picture"
+          />
+          <input
+            type="text"
+            placeholder="What's happening?"
+            className="post-input"
+          />
+          <div className="action-bar">
+            <div className="action-icons-wraper">
+              <div className="action-icons">
+                <InsertPhotoOutlinedIcon />
+              </div>
+              <div className="action-icons">
+                <GifBoxOutlinedIcon />
+              </div>
+              <div className="action-icons">
+                <BallotOutlinedIcon />
+              </div>
+              <div className="action-icons">
+                <SentimentSatisfiedOutlinedIcon />
+              </div>
+              <div className="action-icons">
+                <PendingActionsOutlinedIcon />
+              </div>
+              <div className="action-icons">
+                <LocationOnOutlinedIcon />
+              </div>
+            </div>
+            <div className="post-tweet-button">
+              <div className="post-btn-text">Tweet</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
