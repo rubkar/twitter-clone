8ea83bafcd2134e4e5a558e5be771416ef13e86d
rubkar <ruben.karapetyan96@gmail.com>
2023-03-31 20:31:29 +0500

    rightpanel

--- src/components/rightPane/rightPane.tsx
+++ src/components/rightPane/rightPane.tsx
@@ -1,11 +1,18 @@
 import react from 'react';
 import './rightPane.css';
+import SearchIcon from '@mui/icons-material/Search';
 
 export const RightPane = () => {
-    return (
-      <div className="right-pane-wrapper">
-      dsa
-      </div>
-    );
-  };
-  
+  return (
+    <div className="right-pane-wrapper">
+      <form className="search-bar-wrapper">
+        
+        <input
+          type="search"
+          placeholder="Search Twitter?"
+          className="search-input"
+        > <SearchIcon /></input>
+      </form>
+    </div>
+  );
+};
